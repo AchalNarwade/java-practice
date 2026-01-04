c55a231f92fcd1876e8e52ea77a9ffb1d57ae633
AchalNarwade <achalnarwade211@gmail.com>
2026-01-04 12:43:38 +0530

    Data types

---
+++ dataTypes.java
@@ -0,0 +1,32 @@
+public class dataTypes{
+    public static void main(String[] args){
+
+//PRIMITIVE DATA TYPES
+    byte age = 25;
+    short villagePopulation = 12345;
+    int temp = -3;
+    long totalPopulation = 43562787L;
+    float pi = 3.14f;
+    double avogadro = 6.02214076e23;
+    char grade = 'c';
+   
+    System.out.println("Age: " + age);
+    System.out.println("Village Population: " + villagePopulation);
+    System.out.println("Temperature: " + temp);
+    System.out.println("Total Population: " + totalPopulation);
+    System.out.println("Value of PI: " + pi);
+    System.out.println("Avagadro's Constant: " + avogadro);
+    System.out.println("Grade: " + grade);
+
+
+
+// NON-PRIMITIVE DATA TYPES
+    String name = "Achal";
+    int[] numbers = {1,2,3,4,5};
+ 
+    System.out.println("Name: " + name);
+    System.out.println("First Number: " + numbers[0]);
+
+    }
+
+}
