713355040cdb8d29e3908ae2bbb7c33ab07b7ace
AchalNarwade <achalnarwade211@gmail.com>
2026-01-06 22:58:08 +0530

    Implement FizzBuzz using conditional logic

---
+++ fizzBuzz.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class fizzBuzz{
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter Number: ");
+    int num = sc.nextInt();
+
+    if(num%3==0 && num%5==0){
+        System.out.println("FizzBuzz");
+    }
+    else if(num%5==0){
+        System.out.println("Buzz");
+    }
+    else if(num%3==0){
+        System.out.println("Fizz");
+    }
+    else{
+        System.out.println(num);
+    }
+
+    }
+}
