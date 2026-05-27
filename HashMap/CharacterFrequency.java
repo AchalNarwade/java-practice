542ca5d5b9029181502b45db0d1c65ce202a1608
AchalNarwade <achalnarwade211@gmail.com>
2026-05-27 20:03:55 +0530

    Hashmap problem

---
+++ HashMap/CharacterFrequency.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import java.util.HashMap;
+
+public class CharacterFrequency {
+    public static void main(String args[]){
+        String fruit = "banana";
+        HashMap<Character,Integer> frequency = new HashMap<>();
+        for(int i=0;i<fruit.length();i++){
+            char ch = fruit.charAt(i);
+
+            frequency.put(ch,frequency.getOrDefault(ch,0)+1);
+        }
+
+        System.out.println(frequency);
+    }
+}
